2cc2691bcf1aa2a19c4459756876fb90d7e74350
Ko-de-Koning <110471206+Ko-de-Koning@users.noreply.github.com>
2023-05-24 16:55:24 +0200

    created-rough-layout-home-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+import Link from "next/link";
+import { toEditorSettings } from "typescript";
+import { todo } from "node:test";
+
 export default function Home() {
-  return <h1>Hallo</h1>;
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">Todos</h1>
+        <Link
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          href="/new"
+        >
+          New
+        </Link>
+      </header>
+      <ul className="pl-4"></ul>
+    </>
+  );
 }
